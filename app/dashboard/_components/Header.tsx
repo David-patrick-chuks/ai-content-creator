40197f60cfe740aac9ff2e8e7d4a3186a3c7b08b
Samir Sadek <samirsadek2009@gmail.com>
2024-07-27 05:22:28 +0600

    Design Dashboard Layout

---
+++ app/dashboard/_components/Header.tsx
@@ -0,0 +1,21 @@
+import { Search } from 'lucide-react'
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='p-5 border-b-2 shadow-sm flex justify-between items-center'>
+        
+        <div className='flex gap-2 items-center p-2 border rounded-md max-w-md'>
+            <Search/>
+            <input type="text" placeholder='Search...' className='outline-none' />
+        </div>
+        <div>
+            <h2 className='bg-[#8046FD] text-white p-2 rounded-lg'>
+                Premium Membership
+            </h2>
+        </div>
+    </div>
+  )
+}
+
+export default Header
