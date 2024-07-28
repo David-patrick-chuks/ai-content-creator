90f3e13b1b52ba439f4c2dba8d05d2309e93002b
Samir Sadek <samirsadek2009@gmail.com>
2024-07-29 04:00:55 +0600

    dynamic form Display

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
 
-function CreateNewContent() {
+'use client'
+import React from 'react'
+import FormSection from '../_components/FormSection';
+import OutputSection from '../_components/OutputSection';
+import { ITemplate } from '../../_components/TemplateListSection';
+import Template from '@/app/(data)/Template';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+
+interface PROPS {
+    params:{
+        'template-slug': string
+    }
+}
+
+function CreateNewContent(props:PROPS){
+
+    const selectedTemplate: ITemplate| undefined= Template?.find((item)=>item.slug == props.params['template-slug'])
+    const GenerateAIContent = (formData:any) =>{
+
+    }
+
   return (
-    <div>CreateNewContent</div>
+    <div className='p-10'>
+        <Link href={'/dashboard'}>
+        <Button><ArrowLeft/>Back</Button>
+        </Link>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-5'>
+        {/* Form Section */}
+        <FormSection 
+        userFormInput={(v:any)=>console.log(v)}
+        selectedTemplate={selectedTemplate}/>
+
+        {/* OutputSection */}
+        <div className='col-span-2'>
+        <OutputSection/>
+        </div>
+    </div>
+    </div>
   )
 }
 
