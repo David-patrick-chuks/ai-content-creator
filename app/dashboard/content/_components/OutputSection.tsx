77e09f87c91aad6f5f8844a40bdba04364f7bed0
Samir Sadek <samirsadek2009@gmail.com>
2024-07-30 03:20:56 +0600

    Add rich Editor for showing AI Generated OUTPUT

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
 
 function OutputSection() {
+  const editorRef:any = useRef()
   return (
-    <div>OutputSection</div>
-  )
+    <div className="bg-white shadow-lg border rounded-lg">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="font-bold">Your Result</h2>
+        <Button variant={"outline"} className="flex gap-2"><Copy className="w-4 h-4"/>Copy</Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue="Your result will Appear here"
+        height="600px"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+        onChange={()=>console.log(editorRef.current.getInstance().getMarkdown())}
+      />
+    </div>
+  );
 }
 
-export default OutputSection
+export default OutputSection;
